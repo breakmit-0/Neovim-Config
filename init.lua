aeefe619e71203e8d687162f4c7e2438e20a4b49
breakmit <bonbaoke@gmail.com>
2024-05-22 22:34:49 +0200

    desktop version

--- init.lua
+++ init.lua
@@ -1,47 +1,32 @@
-require("plugins")
-require("remap")
+-- require("plugins")
+-- require("remap")
 
 
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+-- bootstrap lazy.nvim, maybe move to a module later
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+require("lazy").setup {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-tree/nvim-tree.lua", lazy = false, name = "nvim-tree", dependencies = {"nvim-tree/nvim-web-devicons"}}
+}
 
-vim.opt.smartindent = true
-vim.opt.wrap = false
+require("vim_setup")
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+local ps = "plugin_setup."
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+require(ps .. "catpuccin");
+require(ps .. "nvim_tree");
 
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 12
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = ""
-
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
-
-
-vim.g.mapleader = " "
+vim.keymap.set("n","<leader>tt", ":NvimTreeToggle<CR>")
